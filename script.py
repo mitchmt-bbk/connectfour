442330fbd647d4383239ded94a0ab7bcad3e472b
mitchmt-bbk <bbk@301webdesign.com>
2020-03-04 23:19:50 +0000

    Basic layout is complete - responsive, too

--- script.py
+++ script.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 from flask_scss import Scss
 
 app = Flask(__name__)
-# Scss(app, static_dir='styles', asset_dir='assets')
 Scss(app)
-# assets.register('scss_all', scss)
 
 @app.route('/')
 def hello_world():
